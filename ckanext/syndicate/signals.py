dbf426e5a498a3236f51c94d9b06abc18a262c0a
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-03-14 15:40:12 +0200

    Describe signals

--- ckanext/syndicate/signals.py
+++ ckanext/syndicate/signals.py
@@ -7,8 +7,36 @@ except AttributeError:
 
     ckanext = Namespace()
 
-before_syndication = ckanext.signal("syndicate:before_syndication")
-after_syndication = ckanext.signal("syndicate:after_syndication")
+before_syndication = ckanext.signal(u"syndicate:before_syndication")
+"""Sent before package syndication happens.
+Params:
+    sender: local package ID
+    profile: syndication profile
+    details: data that will be sent to the remote portal
+"""
 
-before_group_syndication = ckanext.signal("syndicate:before_group_syndication")
-after_group_syndication = ckanext.signal("syndicate:after_group_syndication")
+after_syndication = ckanext.signal(u"syndicate:after_syndication")
+"""Sent right after package syndication.
+Params:
+    sender: local package ID
+    profile: syndication profile
+    details: remote package details
+"""
+
+before_group_syndication = ckanext.signal(
+    u"syndicate:before_group_syndication"
+)
+"""Sent before group/organization syndication happens.
+Params:
+    sender: local group ID
+    profile: syndication profile
+    details: data that will be sent to the remote portal
+"""
+
+after_group_syndication = ckanext.signal(u"syndicate:after_group_syndication")
+"""Sent right after group/organization syndication.
+Params:
+    sender: local group ID
+    profile: syndication profile
+    remote: remote group details
+"""
